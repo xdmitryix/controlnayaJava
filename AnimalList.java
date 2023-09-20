1fe2b47b282818b5f0e2a01196dbde50972abdd9
dmitry <rbdeman@yandex.ru>
2023-09-20 20:08:39 +0300

    добавил класс AnimalList

---
+++ AnimalList.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AnimalList {
+    public List<Animal> animalsInZoo = new ArrayList<>();
+
+    public void addAnimal(Animal newAnimal){
+        this.animalsInZoo.add(newAnimal);
+    }
+
+
+    
+}
